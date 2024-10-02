baae55f5d092619ec6eb2006c98779f6278e1cee
Rudra Prajapati <rudraprajapati2612@gmail.com>
2024-10-02 14:26:46 +0530

    updated changes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function startMusic() {
-  const bars = document.querySelectorAll(".bar");
-  bars.forEach((bar) => {
-    bar.style.animationPlayState = "running";
-  });
-}
+const menuOpen = document.getElementById("menu-open");
+const menuClose = document.getElementById("menu-close");
+const sidebar = document.querySelector(".container .sidebar");
+
+menuOpen.addEventListener("click", () => (sidebar.style.left = "0"));
+
+menuClose.addEventListener("click", () => (sidebar.style.left = "-100%"));
